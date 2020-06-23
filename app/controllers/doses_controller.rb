96927689fefdc9d0a2ce3d144482ec28a579ec12
Armando Teran <ajteranv@gmail.com>
2020-06-22 21:12:51 -0300

    added new dose

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -5,9 +5,22 @@ class DosesController < ApplicationController
     @dose = Dose.new
   end
 
+  def create
+    @dose = @cocktail.doses.build(dose_params)
+    if @dose.save
+      redirect_to cocktails_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
   private
 
   def find_cocktail
     @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
