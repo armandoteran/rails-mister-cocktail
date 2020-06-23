93061975d58d48ef73b0cceab90c16493a1f4597
Armando Teran <ajteranv@gmail.com>
2020-06-22 21:28:43 -0300

    added destroy

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
   resources :cocktails, only: [:index, :new, :create, :show] do
     resources :doses, only: [:new, :create]
   end
+  resources :doses, only: :destroy
 end
