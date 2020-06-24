35f2900f0600354790afdd607a0156ae77846598
Armando Teran <ajteranv@gmail.com>
2020-06-24 17:35:33 -0300

    added card-product in index and worked on seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,35 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+require "open-uri"
+
 puts 'deleting cocktails'
 Cocktail.destroy_all
 puts 'database is clean'
-puts 'creating cocktails'
-Cocktail.create!(
-  name: 'Daiquiri'
+
+puts 'creating daiquiri'
+daiquiri = Cocktail.create!(name: 'Daiquiri')
+file = URI.open('https://images.unsplash.com/photo-1541288892379-c987c60cd049?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80')
+daiquiri.photo.attach(
+  io: file, filename: 'daiquiri.jpeg', content_type: 'image/jpeg'
 )
-puts 'cocktails created'
+puts 'daiquiri created'
+
+puts 'creating mojito'
+mojito = Cocktail.create!(name: 'Mojito')
+file = URI.open('https://images.unsplash.com/photo-1546171753-97d7676e4602?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1868&q=80')
+mojito.photo.attach(
+  io: file, filename: 'mojito.jpeg', content_type: 'image/jpeg'
+)
+puts 'mojito created'
+
+puts 'creating margarita'
+margarita = Cocktail.create!(name: 'Margarita')
+file = URI.open('https://images.unsplash.com/photo-1557765086-a73d2dc3059e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80')
+margarita.photo.attach(
+  io: file, filename: 'margarita.jpeg', content_type: 'image/jpeg'
+)
+puts 'margarita created'
 
 puts 'deleting ingredients'
 Ingredient.destroy_all
@@ -32,7 +53,19 @@ Ingredient.create!(name: 'Orange liqueur')
 Ingredient.create!(name: 'Agave syrup')
 Ingredient.create!(name: 'Kosher salt')
 Ingredient.create!(name: 'Demerara sugar syrup')
-Ingredient.create!(name: 'Mint spig')
+Ingredient.create!(name: 'Mint sprig')
 Ingredient.create!(name: 'Simple syrup')
+Ingredient.create!(name: 'Water')
+Ingredient.create!(name: 'Vodka')
+Ingredient.create!(name: 'Wiskey')
+Ingredient.create!(name: 'Tomato juice')
+Ingredient.create!(name: 'Balck pepper')
+Ingredient.create!(name: 'Celery stick')
+Ingredient.create!(name: 'Celery salt')
+Ingredient.create!(name: 'Worcestershire sauce')
+Ingredient.create!(name: 'Tabasco')
+Ingredient.create!(name: 'Triple sec')
+Ingredient.create!(name: 'Cointreau')
+Ingredient.create!(name: 'Sugar syrup')
 
 puts 'ingredients created'
