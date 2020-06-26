bb9ee632fbcc8f5fd1bb623e277354a2e6055a84
Armando Teran <ajteranv@gmail.com>
2020-06-25 21:15:22 -0300

    added background image

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@ puts 'database is clean'
 
 puts 'creating daiquiri'
 daiquiri = Cocktail.create!(name: 'Daiquiri')
-file = URI.open('https://images.unsplash.com/photo-1541288892379-c987c60cd049?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80')
+file = URI.open('https://images.unsplash.com/photo-1558950334-8d04704332f8?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80')
 daiquiri.photo.attach(
   io: file, filename: 'daiquiri.jpeg', content_type: 'image/jpeg'
 )
@@ -22,7 +22,7 @@ puts 'daiquiri created'
 
 puts 'creating mojito'
 mojito = Cocktail.create!(name: 'Mojito')
-file = URI.open('https://images.unsplash.com/photo-1546171753-97d7676e4602?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1868&q=80')
+file = URI.open('https://images.unsplash.com/photo-1571390743279-4b4336be29ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80')
 mojito.photo.attach(
   io: file, filename: 'mojito.jpeg', content_type: 'image/jpeg'
 )
@@ -41,20 +41,19 @@ Ingredient.destroy_all
 puts 'database is clean'
 puts 'creating ingredients'
 
-Ingredient.create!(name: 'White rum')
-Ingredient.create!(name: 'Sugar')
-Ingredient.create!(name: 'Lime wheel')
-Ingredient.create!(name: 'Mint leaves')
-Ingredient.create!(name: 'Club soda')
+white_rum = Ingredient.create!(name: 'White rum')
+sugar = Ingredient.create!(name: 'Sugar')
+lime_wheel = Ingredient.create!(name: 'Lime wheel')
+mint_leaves = Ingredient.create!(name: 'Mint leaves')
+club_soda = Ingredient.create!(name: 'Club soda')
 Ingredient.create!(name: 'Ice cubes')
-Ingredient.create!(name: 'Freshly-squeezed lime juice')
-Ingredient.create!(name: 'Tequila')
-Ingredient.create!(name: 'Orange liqueur')
-Ingredient.create!(name: 'Agave syrup')
-Ingredient.create!(name: 'Kosher salt')
+lime_juice = Ingredient.create!(name: 'Freshly-squeezed lime juice')
+tequila = Ingredient.create!(name: 'Tequila')
+agave_syrup = Ingredient.create!(name: 'Agave syrup')
+korsher_salt = Ingredient.create!(name: 'Kosher salt')
 Ingredient.create!(name: 'Demerara sugar syrup')
-Ingredient.create!(name: 'Mint sprig')
-Ingredient.create!(name: 'Simple syrup')
+mint_sprig = Ingredient.create!(name: 'Mint sprig')
+simple_syrup = Ingredient.create!(name: 'Simple syrup')
 Ingredient.create!(name: 'Water')
 Ingredient.create!(name: 'Vodka')
 Ingredient.create!(name: 'Wiskey')
@@ -67,5 +66,103 @@ Ingredient.create!(name: 'Tabasco')
 Ingredient.create!(name: 'Triple sec')
 Ingredient.create!(name: 'Cointreau')
 Ingredient.create!(name: 'Sugar syrup')
+orange_liqueur = Ingredient.create!(name: 'Orange liqueur')
 
 puts 'ingredients created'
+
+puts 'deleting doses'
+Dose.destroy_all
+puts 'database is clean'
+puts 'creating doses'
+
+Dose.create!(
+  description: '1 1/2oz',
+  cocktail_id: daiquiri.id,
+  ingredient_id: white_rum.id
+)
+
+Dose.create!(
+  description: '3/4oz',
+  cocktail_id: daiquiri.id,
+  ingredient_id: lime_juice.id
+)
+
+Dose.create!(
+  description: '1/4oz',
+  cocktail_id: daiquiri.id,
+  ingredient_id: simple_syrup.id
+)
+
+Dose.create!(
+  description: '1',
+  cocktail_id: mojito.id,
+  ingredient_id: lime_juice.id
+)
+
+Dose.create!(
+  description: '1tsp',
+  cocktail_id: mojito.id,
+  ingredient_id: sugar.id
+)
+
+Dose.create!(
+  description: '60ml',
+  cocktail_id: mojito.id,
+  ingredient_id: white_rum.id
+)
+
+Dose.create!(
+  description: 'to taste',
+  cocktail_id: mojito.id,
+  ingredient_id: club_soda.id
+)
+
+Dose.create!(
+  description: '3',
+  cocktail_id: mojito.id,
+  ingredient_id: mint_leaves.id
+)
+
+Dose.create!(
+  description: 'to taste',
+  cocktail_id: mojito.id,
+  ingredient_id: mint_sprig.id
+)
+
+Dose.create!(
+  description: '2oz',
+  cocktail_id: margarita.id,
+  ingredient_id: tequila.id
+)
+
+Dose.create!(
+  description: '1oz',
+  cocktail_id: margarita.id,
+  ingredient_id: lime_juice.id
+)
+
+Dose.create!(
+  description: '1oz',
+  cocktail_id: margarita.id,
+  ingredient_id: orange_liqueur.id
+)
+
+Dose.create!(
+  description: '1/2oz',
+  cocktail_id: margarita.id,
+  ingredient_id: agave_syrup.id
+)
+
+Dose.create!(
+  description: '1',
+  cocktail_id: margarita.id,
+  ingredient_id: lime_wheel.id
+)
+
+Dose.create!(
+  description: 'garnish with',
+  cocktail_id: margarita.id,
+  ingredient_id: korsher_salt.id
+)
+
+puts 'doses created'
